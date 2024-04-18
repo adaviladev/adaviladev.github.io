f442fac7fb8f1f2da48562f10ffa99743f6cf53e
AlvaroDavilaDev <alvarodaviladev@gmail.com>
2024-04-18 18:54:43 -0400

    update

--- FreeCodeCampProjects/02JavaScriptAlgorithmsAndDataStructures(Beta)/12LearnAdvancedArrayMethodsByBuildingAStatisticsCalculator/script.js
+++ FreeCodeCampProjects/02JavaScriptAlgorithmsAndDataStructures(Beta)/12LearnAdvancedArrayMethodsByBuildingAStatisticsCalculator/script.js
@@ -9,14 +9,29 @@ const getMedian = (array) => {
   return median;
 }
 
+// Define una función con un parámetro array que se espera sea un array de números
+const getMode = (array) => {
+// Inicializa un objeto vacío para almacenar el recuento de cada elemento en el array
+  const counts = {};
+// Se utiliza el método .forEach() para iterar sobre cada elemento "el" del array.
+// Dentro del bucle, se incrementa el recuento del elemento "el" en el objeto "counts".
+// Si el elemento ya existe en "counts", se incrementa su recuento en 1; de lo contrario, se inciializa su recuento en 1.
+  array.forEach((el) => {counts[el] = (counts[el] || 0) + 1;})
+
+  if (new Set(Object.values(counts)).size === 1) {
+    return null;
+  }
+}
+
+
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
   const array = value.split(/,\s*/g);
   const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
-
+  
   const mean = getMean(numbers);
-
+  const median = getMedian(numbers);
 
   document.querySelector("#mean").textContent = mean;
-
+  document.querySelector("#median").textContent = median;
 }
