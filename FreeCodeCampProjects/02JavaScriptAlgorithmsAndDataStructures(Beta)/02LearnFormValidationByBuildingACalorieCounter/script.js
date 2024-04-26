8d58fe9800748efbc9f3c91f65797cb4f7ce7504
AlvaroDavilaDev <alvarodaviladev@gmail.com>
2024-04-26 04:02:24 -0400

    update

--- FreeCodeCampProjects/02JavaScriptAlgorithmsAndDataStructures(Beta)/02LearnFormValidationByBuildingACalorieCounter/script.js
+++ FreeCodeCampProjects/02JavaScriptAlgorithmsAndDataStructures(Beta)/02LearnFormValidationByBuildingACalorieCounter/script.js
@@ -98,4 +98,4 @@ function clearForm() {
 
 addEntryButton.addEventListener("click", addEntry);
 calorieCounter.addEventListener("submit", calculateCalories);
-clearButton.addEventListener("click", clearForm);
+clearButton.addEventListener("click", clearForm)
