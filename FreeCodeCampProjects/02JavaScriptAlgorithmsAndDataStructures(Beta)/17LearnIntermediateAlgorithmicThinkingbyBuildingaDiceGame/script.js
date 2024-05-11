26f87340c8e89906a4f6cdca762cf8280d9cef8d
AlvaroDavilaDev <alvarodaviladev@gmail.com>
2024-05-11 02:06:54 -0400

    update

---
+++ FreeCodeCampProjects/02JavaScriptAlgorithmsAndDataStructures(Beta)/17LearnIntermediateAlgorithmicThinkingbyBuildingaDiceGame/script.js
@@ -0,0 +1,6 @@
+const listOfAllDice = document.querySelectorAll(".die");
+const scoreInputs = document.querySelectorAll("#score-options input");
+const scoreSpans = document.querySelectorAll("#score-options span");
+
+const currentRoundText = document.getElementById('current-round');
+const currentRoundRollsText = document.getElementById('current-round-rolls')
