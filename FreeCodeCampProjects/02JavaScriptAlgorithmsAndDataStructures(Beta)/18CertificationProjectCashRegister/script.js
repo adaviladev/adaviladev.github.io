3127083cd772ecc8fab68b7e89935ee981480e5b
AlvaroDavilaDev <alvarodaviladev@gmail.com>
2024-05-14 20:25:59 -0400

    startedPokemonSearchApp

---
+++ FreeCodeCampProjects/02JavaScriptAlgorithmsAndDataStructures(Beta)/18CertificationProjectCashRegister/script.js
@@ -0,0 +1,16 @@
+const userCash = document.getElementById("cash");
+const changeDue = document.getElementById("change-due")
+const purchaseBtn = document.getElementById("purchase-btn")
+
+let price = 1.87;
+let cid = [
+  ["PENNY", 1.01],
+  ["NICKEL", 2.05],
+  ["DIME", 3.1],
+  ["QUARTER", 4.25],
+  ["ONE", 90],
+  ["FIVE", 55],
+  ["TEN", 20],
+  ["TWENTY", 60],
+  ["ONE HUNDRED", 100]
+];
---
+++ FreeCodeCampProjects/02JavaScriptAlgorithmsAndDataStructures(Beta)/20CertificationPokemonSearchApp/script.js
@@ -0,0 +1,14 @@
+const searchInput = document.getElementById("search-input");
+const searchBtn = document.getElementById("search-btn");
+
+const pokemonName = document.getElementById("pokemon-name");
+const pokemonId = document.getElementById("pokemon-id");
+const weight = document.getElementById("weight");
+const height = document.getElementById("height");
+const types = document.getElementById("types");
+const hp = document.getElementById("hp");
+const attack = document.getElementById("attack");
+const defense = document.getElementById("defense");
+const spAttack = document.getElementById("special-attack");
+const spDefense = document.getElementById("special-defense");
+const speed = document.getElementById("speed");
