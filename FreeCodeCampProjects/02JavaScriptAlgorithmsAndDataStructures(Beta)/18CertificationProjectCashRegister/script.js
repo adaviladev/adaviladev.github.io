db0a1af74829635c8a8d8aeb34e739f4336d9c77
AlvaroDavilaDev <alvarodaviladev@gmail.com>
2024-05-15 02:54:27 -0400

    bigupdate

--- FreeCodeCampProjects/02JavaScriptAlgorithmsAndDataStructures(Beta)/18CertificationProjectCashRegister/script.js
+++ FreeCodeCampProjects/02JavaScriptAlgorithmsAndDataStructures(Beta)/18CertificationProjectCashRegister/script.js
@@ -1,16 +1,146 @@
-const userCash = document.getElementById("cash");
-const changeDue = document.getElementById("change-due")
-const purchaseBtn = document.getElementById("purchase-btn")
+const cash = document.getElementById("cash");
+const purchaseBtn = document.getElementById("purchase-btn");
+const changeDue = document.getElementById("change-due");
+const currentChange = document.getElementById("current-change");
+const priceField = document.getElementById("price");
+const totalField = document.getElementById("total")
+
+let total = 0;
+
 
 let price = 1.87;
 let cid = [
-  ["PENNY", 1.01],
-  ["NICKEL", 2.05],
-  ["DIME", 3.1],
-  ["QUARTER", 4.25],
-  ["ONE", 90],
-  ["FIVE", 55],
-  ["TEN", 20],
-  ["TWENTY", 60],
-  ["ONE HUNDRED", 100]
+    ["PENNY", 1.01],
+    ["NICKEL", 2.05],
+    ["DIME", 3.1],
+    ["QUARTER", 4.25],
+    ["ONE", 90],
+    ["FIVE", 55],
+    ["TEN", 20],
+    ["TWENTY", 60],
+    ["ONE HUNDRED", 100]
 ];
+let cidNumbers = [
+    ["0.01", 1.01],
+    ["0.05", 2.05],
+    ["0.1", 3.1],
+    ["0.25", 4.25],
+    ["1", 90],
+    ["5", 55],
+    ["10", 20],
+    ["20", 60],
+    ["100", 100]
+];
+
+window.onload = ()=>{
+    updateCurrentChange();
+    updateTotal();
+    updatePrice();
+}
+
+const updateCurrentChange = ()=>{
+    let stringHtml= "<table><thead><tr><th>Different Coins</th><th>Amount</th></tr></thead><tbody>";
+    for(const money of cid){
+        stringHtml += `<tr><td>${money[0]}</td><td>${money[1]}</td></tr>`
+    }
+    stringHtml += "</tbody></table>"
+    currentChange.innerHTML = stringHtml;
+}
+
+const updateTotal = ()=>{
+    total = 0;
+    for(const money of cid){
+        total += money[1];
+    }
+    total = Number(total.toFixed(2));
+    totalField.textContent = `$${total}`;
+}
+
+const updatePrice = () =>{
+    priceField.textContent = `$${price}`;
+}
+
+const buying = ()=> {
+    let paid = parseFloat(cash.value);
+
+    if(paid < price) {
+        alert("Customer does not have enough money to purchase the item");
+        updateCurrentChange();
+        updateTotal();
+        updatePrice();
+        return;
+    }
+    if(paid === price){
+        changeDue.textContent = "No change due - customer paid with exact cash";
+        price = 0;
+        updateCurrentChange();
+        updateTotal();
+        updatePrice();
+    }else if(parseFloat((paid - price).toFixed(2)) === total){
+        changeDue.textContent = "Status: CLOSED"
+        for(let value of cid){
+            if(value[1] !== 0){
+                changeDue.textContent += ` ${value[0]}: $${value[1].toFixed(2)}`;
+                value[1] = 0;
+            }
+        }
+        updateCurrentChange();
+        updateTotal();
+        updatePrice();
+    }
+    else if(paid - price  > total){
+        changeDue.textContent = "Status: INSUFFICIENT_FUNDS"
+    }else{
+        const copyArray = cid.slice();
+        const changeMoney = Number((paid - price).toFixed(2));
+
+        paid = changeMoney;
+        console.log(paid)
+        const potentialChange = {}
+        const potentialArr = []
+        for(let i = cid.length - 1; i>= 0; i--){
+            while(paid >= Number(cidNumbers[i][0]) && parseFloat((copyArray[i][1] - Number(cidNumbers[i][0] )).toFixed(2))>= 0){
+                paid = Number((paid-Number(cidNumbers[i][0])).toFixed(2));
+                console.log(paid)
+                if(potentialChange[cid[i][0]]){
+                    potentialChange[cid[i][0]] = parseFloat((potentialChange[cid[i][0]] + Number(cidNumbers[i][0])).toFixed(2));
+                }else{
+                    potentialChange[cid[i][0]] = Number(cidNumbers[i][0])
+                }
+                copyArray[i][1] =Number((copyArray[i][1]-Number(cidNumbers[i][0])).toFixed(2));
+            }
+        }
+
+        for(let value of Object.keys(potentialChange)){
+            const arrEntry = [value,parseFloat(potentialChange[value].toFixed(2))];
+            potentialArr.push(arrEntry)
+            console.log(arrEntry)
+        }
+
+        let potentialTotal = 0;
+        for(let value of potentialArr){
+            potentialTotal += value[1];
+        }
+        potentialTotal = Number(potentialTotal.toFixed(2))
+
+        if(potentialTotal !== changeMoney ){
+            changeDue.textContent = "Status: INSUFFICIENT_FUNDS";
+            return;
+        }
+        cid = copyArray;
+
+        changeDue.textContent = `Status: OPEN`;
+        for(let value of potentialArr){
+            changeDue.textContent += ` ${value[0]}: $${value[1]}`;
+        }
+        updateCurrentChange();
+        updateTotal();
+        updatePrice();
+
+    }
+}
+
+purchaseBtn.addEventListener("click", updateCurrentChange)
+purchaseBtn.addEventListener("click", updateTotal)
+purchaseBtn.addEventListener("click", updatePrice)
+purchaseBtn.addEventListener("click", buying)
--- FreeCodeCampProjects/02JavaScriptAlgorithmsAndDataStructures(Beta)/20CertificationPokemonSearchApp/script.js
+++ FreeCodeCampProjects/02JavaScriptAlgorithmsAndDataStructures(Beta)/20CertificationPokemonSearchApp/script.js
@@ -1,5 +1,8 @@
 const searchInput = document.getElementById("search-input");
-const searchBtn = document.getElementById("search-btn");
+const searchBtn = document.getElementById("search-button");
+const searchForm = document.getElementById("search-form");
+const spriteContainer = document.getElementById("sprite-container");
+const searchButtonRandom = document.getElementById("search-button-random")
 
 const pokemonName = document.getElementById("pokemon-name");
 const pokemonId = document.getElementById("pokemon-id");
@@ -12,3 +15,93 @@ const defense = document.getElementById("defense");
 const spAttack = document.getElementById("special-attack");
 const spDefense = document.getElementById("special-defense");
 const speed = document.getElementById("speed");
+
+const getPokemon = async () => {
+    try {
+        const pokemonNameOrId = searchInput.value.toLowerCase();
+        const response = await fetch(
+            `https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${pokemonNameOrId}`
+        );
+        const data = await response.json();
+
+        //pokemon info
+        pokemonName.textContent = `${data.name.toUpperCase()}`;
+        pokemonId.textContent = `#${data.id}`;
+        weight.textContent = `Weight: ${data.weight}`;
+        height.textContent = `Height: ${data.height}`;
+        spriteContainer.innerHTML = `
+          <img id="sprite" src="${data.sprites.front_default}" alt="${data.name} front default sprite">
+        `;
+
+        //pokemon stats
+        hp.textContent = `${data.stats[0].base_stat}`;
+        attack.textContent = `${data.stats[1].base_stat}`;
+        defense.textContent = `${data.stats[2].base_stat}`;
+        spAttack.textContent = `${data.stats[3].base_stat}`;
+        spDefense.textContent = `${data.stats[4].base_stat}`;
+        speed.textContent = `${data.stats[5].base_stat}`;
+
+        //types
+        types.innerHTML = "Type: " + data.types.map(obj => `<span class="type ${obj.type.name}">${obj.type.name}</span>`).join('');
+
+    }   catch (err) {
+        alert("Pokémon not found")
+    }
+}
+
+const getRandomPokemon = async () => {
+    //10001 - 10277
+    const randomId = Math.random() < 0.5 ?
+        Math.floor(Math.random() * 1025) + 1 :
+        Math.floor(Math.random() * (10277 - 10001 + 1)) + 10001;
+    try {
+        const response = await fetch(
+            `https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${randomId}`
+        );
+        const data = await response.json();
+        // Display random Pokémon
+        pokemonName.textContent = `${data.name.toUpperCase()}`;
+        pokemonId.textContent = `#${data.id}`;
+        weight.textContent = `Weight: ${data.weight}`;
+        height.textContent = `Height: ${data.height}`;
+        spriteContainer.innerHTML = `
+          <img id="sprite" src="${data.sprites.front_default}" alt="${data.name} front default sprite">
+        `;
+        hp.textContent = `${data.stats[0].base_stat}`;
+        attack.textContent = `${data.stats[1].base_stat}`;
+        defense.textContent = `${data.stats[2].base_stat}`;
+        spAttack.textContent = `${data.stats[3].base_stat}`;
+        spDefense.textContent = `${data.stats[4].base_stat}`;
+        speed.textContent = `${data.stats[5].base_stat}`;
+
+        types.innerHTML = "Type: " + data.types.map(obj => `<span class="type ${obj.type.name}">${obj.type.name}</span>`).join('');
+        
+    } catch (err) {
+        alert("Failed to fetch random Pokémon");
+    }
+};
+
+searchButtonRandom.addEventListener("click", getRandomPokemon);
+
+const resetDisplay = () => {
+    const sprite = document.getElementById('sprite');
+    if (sprite) sprite.remove();
+  
+    // reset stats
+    pokemonName.textContent = '';
+    pokemonId.textContent = '';
+    types.innerHTML = '';
+    height.textContent = '';
+    weight.textContent = '';
+    hp.textContent = '';
+    attack.textContent = '';
+    defense.textContent = '';
+    specialAttack.textContent = '';
+    specialDefense.textContent = '';
+    speed.textContent = '';
+  };
+  
+  searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    getPokemon();
+  });
